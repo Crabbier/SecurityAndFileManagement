2a1d2c983b6bf13e103441564cd5c23c6420aeff
Carlos Francisco Serrano Prado <carlos.serrano.prado@gmail.com>
2023-12-01 18:17:33 -0500

    Files Update
    editableView.php created and updated with project information.
    
    signIn-Up.php file renamed as signInUp.php

--- editableView.php
+++ editableView.php
@@ -1,7 +1,7 @@
 
 <!-- IF ADMINISTRATOR IS LOGGED IN WE SHOW THE INFORMATION -->
 <?php
-	require './inc/header.php';
+	require './include/header.php';
 	//check for authentication before we show any data
 	session_start();
 	if (!isset($_SESSION['user_id'])) {
@@ -9,42 +9,14 @@
 		exit();
 	}
 	else {
-		//connect to db
-		require './inc/database.php';
-		//set up query
-		$sql = "SELECT * FROM phppeople";
-		//run the query and store the results
-		$result = $conn->query($sql);
-		//start our table
-		echo '<section class="person-row">';
-		echo '<table class="table table-striped">
-						<tr>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Email</th>
-							<th>Phone Number</th>
-						</tr>';
-		foreach ($result as $row) {
-			echo '<tr>
-							<td>' . $row['fname']  . '</td>
-							<td>' . $row['lname']  . '</td>
-							<td>' . $row['email']  . '</td>
-							<td>' . $row['telNumber']  . '</td>
-					</tr>';
-			}
-		//close the table
-		echo '</table>';
-		echo '<a class="btn btn-warning" href="logout.php">Logout</a>';
-		echo '</section>';
-
-		//disconnect
-		$conn = null;
+    
 	}
-	require './inc/footer.php';
+	require './include/footer.php';
 ?>
 <?php
   // Include database file
-  include 'database.php';
+  require_once('database.php');
+	$res = $database->read_products();
   //$customerObj = new database();
   // Delete record from table
   if(isset($_GET['deleteId']) && !empty($_GET['deleteId'])) {
@@ -55,28 +27,7 @@
 
 <!-- EDIT - DELETE VIEW -->
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD With PHP</title>
-    <meta name="description" content="This week we will be building a CREATE and READ CRUD system with PDO">
-    <meta name="robots" content="noindex, nofollow">
-    <!--  Add our Google fonts  -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
-    <!--  Add our Bootstrap  -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" >
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!--  Add our custom CSS  -->
-    
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
-    <link rel="stylesheet" href="./css/style.css">
-  </head>
-  <body>
+<?php require ('./include/header.php'); ?>
   <header>
     <h1>Full CRUD With OOP</h1>
   </header>
@@ -107,32 +58,35 @@
       </h2>
       <table class="table table-hover table-dark table-striped">
         <thead>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Salary</th>
-          <th>Action</th>
-        </tr>
+          <tr>
+            <th>#</th>
+            <th>Watch Name</th>
+            <th>Category</th>
+            <th>Brand</th>
+            <th>Description</th>
+            <th>Registration Date</th>
+          </tr>
         </thead>
         <tbody>
         <?php
        // session_start(); 
        
-        $customers = $customerObj->displayData();
-        if ($customers!=null){
-        foreach ($customers as $customer) {
+        $products = $database->read_products();
+        if ($products!=null){
+        foreach ($products as $product) {
         //  $_SESSION['valueToPass'] = $customer['id'];
           ?>
           <tr>
-            <td><?php echo $customer['id'] ?></td>
-            <td><?php echo $customer['name'] ?></td>
-            <td><?php echo $customer['email'] ?></td>
-            <td><?php echo $customer['salary'] ?></td>
+            <td><?php echo $product['id']; ?></td>
+            <td><?php echo $product['name'], " ", $r['color']; ?></td>
+            <td><?php echo $product['category'] ?></td>
+            <td><?php echo $product['brand'] ?></td>
+            <td><?php echo $product['description'] ?></td>
+            <td><?php echo $product['date'] ?></td>
             <td>
-              <button class="btn btn-danger"><a href="edit.php?editId=<?php echo $customer['id'] ?>">
+              <button class="btn btn-danger"><a href="edit.php?editId=<?php echo $product['id'] ?>">
                   <i class="fa fa-pencil text-white" ></i></a></button>
-              <button class="btn btn-danger"><a href="index.php?deleteId=<?php echo $customer['id'] ?>" onclick="return confirm('Are you sure?'); return false;">
+              <button class="btn btn-danger"><a href="index.php?deleteId=<?php echo $product['id'] ?>" onclick="return confirm('Are you sure?'); return false;">
                   <i class="fa fa-trash text-white" ></i>
                 </a></button>
             </td>
@@ -142,6 +96,5 @@
       </table>
     </section>
 </main>
-</body>
-</html>
+<?php require ('./include/footer.php'); ?>
 
